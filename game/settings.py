de9afa1bd3f1b6388c8a79a7e15ae12c4e2f7cbd
Oleh Fedorenko <56025512+OleFendor@users.noreply.github.com>
2020-06-04 20:15:37 +0300

    fixed

--- game/settings.py
+++ game/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
